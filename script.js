3797004c7a16274d7c748fbc0dbecc0caf82e4d6
Kabir <60716161+Mohkabir@users.noreply.github.com>
2021-02-26 08:59:25 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $("document").ready(function(){
-  $(".fa-bars").click(function(){
+  $(".nav-icon").click(function(){
     $("#main_navigation").toggle(500);
   });
 });
+/*
+let navIconWrapper = document.querySelector(".nav-icon");
+let navIcon = document.querySelector(".fa-bars");
+
+navIconWrapper.addEventListener("click", function(){
+  navIcon.classList.toggle("fa fa-window-close");
+})
+*/
+
+
 
